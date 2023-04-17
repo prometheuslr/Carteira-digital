3e40c2b7453ec3c958f5ad4c41318e7e8179c99f
prometheuslr <prometheus.inves@gmail.com>
2023-04-16 23:11:39 -0300

    ajuste linha 4

--- financas/finamcas/src/App.java
+++ financas/finamcas/src/App.java
@@ -2,7 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Saldo saldo01 = new Saldo();
             saldo01.valorSaldo = 1000;
-            saldo01.valorSaldo = 1000 ;
         saldo01.mostraSaldo();
 
         Gasto gasto1 = new Gasto();
