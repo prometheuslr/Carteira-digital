e9606172eb05018946d7c966ce859d5999262ce8
Lhuuk <106386505+Lhuuk@users.noreply.github.com>
2023-06-05 14:19:35 -0300

    Update Saldo.java

--- src/model/Saldo.java
+++ src/model/Saldo.java
@@ -1,8 +1,15 @@
 package model;
 
+//classe "Saldo" que vai herdar as caracteristicas da classe "Salario"
 public class Saldo extends Salario{
+    
+     /* 
+    modificador de acesso "protected" que torna o membro acessível 
+    às classes do mesmo pacote ou através de herança
+    */  
     protected double valorSaldo;
-
+    
+    //definindo o valor inicial do saldo no caso "0.0"
     public Saldo() {
         this.valorSaldo = 0.0;
     }
@@ -17,11 +24,13 @@ public class Saldo extends Salario{
     public void setValorSaldo(double valorSaldo) {
         this.valorSaldo = valorSaldo;
     }
-
+    
+    //adiciona um valor ao saldo atual
     public void addSaldo(double newSaldo){
         
     }
-
+    
+    //subtrai um valor do saldo atual
     public void subSaldo(double sub){
         
     }
