ee378b68e13fc18e009c4a77af108cbb3e018966
Lhuuk <106386505+Lhuuk@users.noreply.github.com>
2023-06-05 14:48:39 -0300

    Update BoletoMensal.java

--- src/model/BoletoMensal.java
+++ src/model/BoletoMensal.java
@@ -1,9 +1,9 @@
 package model;
 
-// Classe "Debitol" que herda as características da classe "Despesa"
+// Classe "BoletoMensal" que herda as características da classe "Despesa"
 public class BoletoMensal extends Despesa{
 
-    // Construtor da classe "Debito" que recebe o nome e valor 
+    // Construtor da classe "BoletoMensal" que recebe o nome e valor 
     public BoletoMensal(String nome, double valor) {
         super(nome, valor);
         
