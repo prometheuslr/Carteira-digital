a940f884ae7fd78107c501dce7ef1e5f8fd8ae0c
Lhuuk <106386505+Lhuuk@users.noreply.github.com>
2023-06-05 14:17:47 -0300

    Update Salario.java

--- src/model/Salario.java
+++ src/model/Salario.java
@@ -1,22 +1,24 @@
 package model;
 
+//classe "Salario" que possui dois atributos "valoSalari"o e "diaPagamento"
 public class Salario {
     double valoSalario;
     int diaPagamento;
 
-
+    //getter de diaPagamento
     public int getDiaPagamento() {
         return diaPagamento;
     }
-
+    
+    //setter de diaPagamento
     public void setDiaPagamento(int diaPagamento) {
         this.diaPagamento = diaPagamento;
     }
-
+    //getter de valoSalario
     public double getValoSalario() {
         return valoSalario;
     }
-
+    //setter de valoSalario
     public double setValoSalario(double valoSalario) {
         this.valoSalario = valoSalario;
         return this.valoSalario;
