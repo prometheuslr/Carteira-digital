930d862d2a331860e43a5573e2b615c0bcf9d888
Lhuuk <106386505+Lhuuk@users.noreply.github.com>
2023-06-05 23:25:56 -0300

    Update Debito.java

--- src/model/Debito.java
+++ src/model/Debito.java
@@ -1,11 +1,13 @@
 package model;
 
 public class Debito extends Despesa {
+    // Construtor da classe que recebe o nome e valor do débito
     public Debito(String nome, double valor) {
         super(nome, valor);
     }
 
     @Override
+    // Sobrescrevendo o método getTipo() da classe pai
     public String getTipo() {
         return "Débito";
     }
