a5be1a9df50f80447a363e7961bde1f43c850809
Lhuuk <106386505+Lhuuk@users.noreply.github.com>
2023-06-05 23:27:32 -0300

    Update Despesa.java

--- src/model/Despesa.java
+++ src/model/Despesa.java
@@ -6,18 +6,22 @@ public abstract class Despesa {
     private String nome;
     private double valor;
 
+     // Construtor da classe que recebe o nome e valor da despesa
     public Despesa(String nome, double valor) {
         this.nome = nome;
         this.valor = valor;
     }
 
+     // Construtor da classe que recebe o nome e valor da despesa
     public String getNome() {
         return nome;
     }
 
+    // Getter para obter o valor da despesa
     public double getValor() {
         return valor;
     }
     
+    // Método abstrato para obter o tipo da despesa
     public abstract String getTipo();
 }
